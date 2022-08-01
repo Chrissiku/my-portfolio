1dd2c21554b73d9fe53d94441369f28ab17d28d9
chrissiku <chrissiku5@gmail.com>
2022-08-01 23:21:48 +0200

    Implement header for the desktop view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,58 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
-import Navigation from "./NavMenu";
+import Icon from "react-icons-kit";
+import linksArray from "./headerData";
+import { thLarge } from "react-icons-kit/fa/thLarge";
+import { close } from "react-icons-kit/fa/close";
 
 const Header = () => {
+  const navigationRef = useRef();
+  const toggleNavbar = () => {
+    navigationRef.current.classList.toggle("mobile-nav");
+  };
   return (
     <>
       <header>
         <Link to="/">
-          <a href="/" className="logo">
-            <span>&lt;</span>&nbsp;Chris Siku&nbsp;<span>/&gt;</span>
-          </a>
+          <span className="logo">Chris Siku</span>
         </Link>
-        <Navigation />
+        <nav ref={navigationRef}>
+          <ul>
+            {linksArray.map((menuItem) => (
+              <Link to={menuItem.path} key={menuItem.id}>
+                <li onClick={toggleNavbar}>
+                  <i>{menuItem.icon}</i>
+                  <span>{menuItem.menu}</span>
+                </li>
+              </Link>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="menu-icon"
+            id="menu-icon close-navbar"
+            onClick={toggleNavbar}
+          >
+            <Icon icon={close} size={20} />
+          </button>
+
+          <div className="button">
+            <button type="button" className="btn">
+              Download CV
+            </button>
+          </div>
+        </nav>
+
+        <button
+          type="button"
+          className="menu-icon"
+          id="menu-icon"
+          onClick={toggleNavbar}
+        >
+          <Icon icon={thLarge} size={20} />
+        </button>
       </header>
     </>
   );
 };
-
 export default Header;
--- src/Components/Header/headerData.js
+++ src/Components/Header/headerData.js
@@ -1,18 +1,35 @@
+import React from "react";
+import { v4 as menuId } from "uuid";
+import Icon from "react-icons-kit";
+import { home } from "react-icons-kit/fa/home";
+
+const ICON_SIZE = 20;
+
 const linksArray = [
   {
-    id: 1,
-    item: "Home",
+    id: menuId(),
+    menu: "Home",
     path: "/",
+    icon: <Icon icon={home} size={ICON_SIZE} />,
   },
   {
-    id: 2,
-    item: "About",
+    id: menuId(),
+    menu: "About",
     path: "/",
+    icon: <Icon icon={home} size={ICON_SIZE} />,
   },
   {
-    id: 3,
-    item: "Work",
+    id: menuId(),
+    menu: "Work",
     path: "/",
+    icon: <Icon icon={home} size={ICON_SIZE} />,
+  },
+  
+  {
+    id: menuId(),
+    menu: "contact",
+    path: "/",
+    icon: <Icon icon={home} size={ICON_SIZE} />,
   },
 ];
 export default linksArray;
