a3367d5b0981709b8efd09ccd3152b03b5a9fcc6
chrissiku <chrissiku5@gmail.com>
2022-08-02 17:52:23 +0200

    Implement home section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,25 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <section className="home">hello</section>
+      <section className="home">
+        <div className="dev-info">
+          <h1>Hi, I'm Chris Siku</h1>
+          <h2>A Software Developer</h2>
+        </div>
+        <p>
+          I can help you build a product , feature or website Look through some
+          of my work and experience! If you like what you see and have a project
+          you need coded, don’t hestiate to contact me.
+        </p>
+        <div className="buttons">
+          <button type="buton" className="btn">
+            Contact
+          </button>
+          <button type="buton" className="btn">
+            Download Cv
+          </button>
+        </div>
+      </section>
     </>
   );
 };
