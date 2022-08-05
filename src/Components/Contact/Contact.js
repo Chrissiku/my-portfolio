2c828050c930a3e8c3003c0ee5af78d6b7fc7e9c
chrissiku <chrissiku5@gmail.com>
2022-08-05 18:58:04 +0200

    Set Code to DRY

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,23 +1,64 @@
 import React from "react";
 import Icon from "react-icons-kit";
 import { thLarge } from "react-icons-kit/fa/thLarge";
+import icon from "../Home/Icon";
+import ParticlesBg from "particles-bg";
 
 const Contact = () => {
+  let config = {
+    num: [4, 4],
+    rps: 0.1,
+    radius: [5, 40],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-50, 50],
+    alpha: [0.6, 0],
+    scale: [0.1, 0.9],
+    body: icon,
+    position: "all",
+    cross: "dead",
+    random: 10,
+  };
   return (
     <>
       <section className="contact">
+        <ParticlesBg type="custom" config={config} bg={true} />
         <h2 className="title">Contact</h2>
         <div className="contact-cards">
           <div className="contact-info">
             <div className="single-contact">
-              <Icon icon={thLarge} size={25} />
+              <Icon icon={thLarge} size={20} />
+              <div className="info">
+                <span>Phone</span>
+                <a href="tel:+243992984499">+243992984499</a>
+              </div>
+            </div>
+
+            <div className="single-contact">
+              <Icon icon={thLarge} size={20} />
+              <div className="info">
+                <span>Phone</span>
+                <a href="tel:+243992984499">+243992984499</a>
+              </div>
+            </div>
+
+            <div className="single-contact">
+              <Icon icon={thLarge} size={20} />
+              <div className="info">
+                <span>Phone</span>
+                <a href="tel:+243992984499">+243992984499</a>
+              </div>
+            </div>
+
+            <div className="single-contact">
+              <Icon icon={thLarge} size={20} />
               <div className="info">
                 <span>Phone</span>
                 <a href="tel:+243992984499">+243992984499</a>
               </div>
             </div>
             <div className="single-contact">
-              <Icon icon={thLarge} size={25} />
+              <Icon icon={thLarge} size={20} />
               <div className="info">
                 <span>Phone</span>
                 <a href="mailto:chrissiku5@gmail.com">chrissiku5@gmail.com</a>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,17 +1,19 @@
 import Icon from "react-icons-kit";
 import { thLarge } from "react-icons-kit/fa/thLarge";
+import ParticlesBg from "particles-bg";
 
 const Footer = () => {
   return (
     <>
       <section className="footer">
+        <ParticlesBg num={20} type="cobweb" bg={true} color="#0ab4e7"/>
         <div className="social">
-          <Icon icon={thLarge} />
-          <Icon icon={thLarge} />
-          <Icon icon={thLarge} />
-          <Icon icon={thLarge} />
-          <Icon icon={thLarge} />
-          <Icon icon={thLarge} />
+          <Icon icon={thLarge} size={20} />
+          <Icon icon={thLarge} size={20} />
+          <Icon icon={thLarge} size={20} />
+          <Icon icon={thLarge} size={20} />
+          <Icon icon={thLarge} size={20} />
+          <Icon icon={thLarge} size={20} />
           <a href="mailto:chrissik5@gmail.com">chrissik5@gmail.com</a>
         </div>
         <span>&copy;All Right reserved | Chris Siku</span>
