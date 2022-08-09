a9c48142f286e22e2655e529d3b5d5c8ff1c7e1c
Chris Siku <chrissiku5@gmail.com>
2022-08-09 18:21:56 +0200

    Update testimonialData.js

--- src/Components/Testimonial/testimonialData.js
+++ src/Components/Testimonial/testimonialData.js
@@ -11,7 +11,7 @@ const TestimonialData = [
   {
     id: Id(),
     picture:
-      'https://pbs.twimg.com/profile_images/1554508793224073217/5PnA54dx_400x400.jpg',
+      'https://user-images.githubusercontent.com/101924220/183704376-0552e165-500c-4c13-80ac-8d98a4b96988.jpg',
     name: 'Sanja Mandic',
     recomandation:
       "Christian is a fantastic software engineer, it was my pleasure to work with him, as he was doing everything on time and he is detail-oriented. We pair-programmed together in Microverse program and during that time his work ethic blew me away. Christian reads clearly the whole needed documentation and his attention to detail is evident in all of the projects I've worked with him on, he had really great ideas on how to improve UI. He's also comfortable to work with, as he's always positive, and now I see him as my friend. I can't recommend him enough!",
@@ -46,7 +46,7 @@ const TestimonialData = [
   {
     id: Id(),
     picture:
-      'https://pbs.twimg.com/profile_images/1550549731088162822/1K8Uq2wQ_400x400.jpg',
+      'https://user-images.githubusercontent.com/101924220/183704228-b64b7bf0-e192-46bf-938f-4598b5f3ebc9.jpg',
     name: 'Favour Amarachi Ezeugwa',
     recomandation:
       'Chris has been an outstanding software developer. Having worked with him on some projects, I must say he is a development guru. His work is always top-notch, and he always goes the extra mile to ensure the project requirements are met. He is a highly-skilled, experienced, and helpful professional with a positive attitude. Working with Chris has been a great experience for me as I learned many hard and soft skills from him. Thanks, Chris.',
@@ -80,7 +80,7 @@ const TestimonialData = [
 
   {
     id: Id(),
-    picture: 'https://avatars.githubusercontent.com/u/100236377?v=4',
+    picture: 'https://user-images.githubusercontent.com/101924220/183704294-bf22d575-ec38-4494-b7f4-3c91b37aa75c.jpg',
     name: 'Silvia Tofana',
     recomandation:
       'He has good experience in the workings of how to handle works regarding servers, databases, and also system engineering. He also has good team management abilities and is a really dedicated worker. So I would really like to recommend him for the post of full stack developer of your company. I can assure you that, he would be a great option for helping your company in its future growth and success. Thank you for taking the time to read this recommendation from your busy schedule.',
@@ -109,7 +109,7 @@ const TestimonialData = [
   {
     id: Id(),
     picture:
-      'https://pbs.twimg.com/profile_images/1508840180320550919/09Kc0uHf_400x400.jpg',
+      'https://user-images.githubusercontent.com/101924220/183703261-51a3ea35-b7c2-4207-953c-9f5ded90eb60.jpg',
     name: 'Grace Muthui',
     recomandation:
       "Working with Christian is always fun and also an amazing learning experience as he portrays a lot of dedication and also extensive mastery when it comes to software development. Emphasizing on his technical skills we get to learn a lot from each other and I admire his problem solving skills and his strategic way of thinking when it comes to a given task. Also his soft skills are top notch and I hope to work with him again. I recommend him 100% for he is really a good asset on anyone's team.",
