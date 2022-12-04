3c5115daa3cdab12c6ed1cde67f288f35552026b
Chris Siku <chrissiku5@gmail.com>
2022-12-04 14:49:41 +0200

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,7 +13,13 @@ const About = () => (
         <div className="about-text">
           <h3>Who Am I</h3>
           <p>
-            Full-stack software developer and holder of a degree, Bachelor&apos;s degree,  in computer science, I am a bilingual and transversal profile with +3 years of experience in the development of web applications and embedded electronics. Sociable and determined, I pursue innovation across the world and its cultures. Organize, renew and learn are my watchwords. Something I can't do? I learn it!
+            Full-stack software developer and holder of a degree,
+            Bachelor&apos;s degree, in computer science, I am a bilingual and
+            transversal profile with +3 years of experience in the development
+            of web applications and embedded electronics. Sociable and
+            determined, I pursue innovation across the world and its cultures.
+            Organize, renew and learn are my watchwords. Something I can`&apos;t do? I
+            learn it!
           </p>
         </div>
       </div>
